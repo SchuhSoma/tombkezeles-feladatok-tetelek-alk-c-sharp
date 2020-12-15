59daa7de9c741f5b13238457626c4d32a6f91e5a
SchuhSoma <60043511+SchuhSoma@users.noreply.github.com>
2020-12-15 11:30:02 +0100

    Add files via upload

---
+++ ConsoleApp7/ConsoleApp7/Program.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static int[] KesesekTMB = new int[32];
+        static int[] Munkaido=new int[21];
+        static Random rnd = new Random();
+        static void Main(string[] args)
+        {
+            KesesekFeladat();
+            MunkaidoFeladat();
+            Console.ReadKey();
+        }
+
+        private static void MunkaidoFeladat()
+        {
+            Feladat1Munk(); Console.WriteLine("\n--------------------------------\n");
+            Feladat3Munk(); Console.WriteLine("\n--------------------------------\n");
+            Feladat4Munk(); Console.WriteLine("\n--------------------------------\n");
+            Feladat56Munk(); Console.WriteLine("\n--------------------------------\n");
+            Feladat7Munk(); Console.WriteLine("\n--------------------------------\n");
+            Feladat8Munk(); Console.WriteLine("\n--------------------------------\n");
+            Feladat9Munk(); Console.WriteLine("\n--------------------------------\n");
+        }
+
+        private static void Feladat9Munk()
+        {
+            Console.WriteLine("Írjuk ki az értékeket növekvő rendben!");
+            int CsereMunka;
+            for (int i = 0; i < Munkaido.Length - 1; i++)
+            {
+                for (int j = 0; j < Munkaido.Length - 1; j++)
+                {
+                    if (Munkaido[j] > Munkaido[j + 1])
+                    {
+                        CsereMunka = Munkaido[j];
+                        Munkaido[j] = Munkaido[j + 1];
+                        Munkaido[j + 1] = CsereMunka;
+                    }
+                }
+            }
+            for (int j = 0; j < Munkaido.Length; j++)
+            {
+                Console.WriteLine("\t{0}", Munkaido[j]);
+            }
+        }
+
+        private static void Feladat8Munk()
+        {
+            Console.WriteLine("Hányszor volt a napi munkaidő 11 és13 óra közötti?");
+            int dbVolt = 0;
+            for (int i = 0; i < Munkaido.Length; i++)
+            {
+                if(Munkaido[i]>11 && Munkaido[i]<13)
+                {
+                    dbVolt++;
+                }
+            }
+            Console.WriteLine("\tEnnyi elkalommal volt a napi munkaidő 11 és13 óra közötti: {0} ",dbVolt);
+        }
+
+        private static void Feladat7Munk()
+        {
+            Console.WriteLine("Volt-e 9 órás munka napja valamikor?");
+            bool MunkaDont = false;
+            for (int i = 0; i < Munkaido.Length; i++)
+            {
+                if(Munkaido[i]==9)
+                { MunkaDont = true; }
+            }
+            if (MunkaDont == true) Console.WriteLine("\tVolt 9 órája");
+            else Console.WriteLine("\tNem volt 9 órája");
+
+        }
+
+        private static void Feladat56Munk()
+        {
+            Console.WriteLine("Melyik nap dolgozott legtöbbet illetve legkevesebbet? ");
+            int MaxMunka = int.MinValue;
+            int MaxHely = 0;
+            int MinMunka = int.MaxValue;
+            int MinHely = 0;
+            for (int i = 0; i < Munkaido.Length; i++)
+            {
+                if(Munkaido[i]>MaxMunka)
+                {
+                    MaxMunka = Munkaido[i];
+                    MaxHely = i;
+                }
+                if(Munkaido[i]<MinMunka)
+                {
+                    MinMunka = Munkaido[i];
+                    MinHely = i;
+                }
+            }
+            Console.WriteLine("\tLegkevesebbet ezen a napon dolgozott :{0} -> ennyi órát dolgozott aznap :{1}", MinHely+1, MinMunka);
+            Console.WriteLine("\tLegtöbbet ezen a napon dolgozott :{0} -> ennyi órát dolgozott aznap :{1}", MaxHely+1, MaxMunka);
+        }
+
+        private static void Feladat4Munk()
+        {
+            Console.WriteLine("Hányszor dolgozott 8 órát?");
+            int dbMunk = 0;
+            for (int i = 0; i < Munkaido.Length; i++)
+            {
+                if(Munkaido[i]==8)
+                {
+                    dbMunk++;
+                }
+            }
+            Console.WriteLine("\tEnnyi alkalommal volt 8 órája: {0}", dbMunk);
+        }
+
+        private static void Feladat3Munk()
+        {
+            Console.WriteLine("Adjuk meg, hogy mennyi pénzt keresett a tanár, ha nettó 2500 Ft - ot kap egy óráért!");
+            int OsszegMunka = 0;
+            for (int i = 0; i < Munkaido.Length; i++)
+            {
+                OsszegMunka += Munkaido[i];
+            }
+            int Fizetes = OsszegMunka * 2500;
+            Console.WriteLine("\tA tanár ennyit keresett három hét alatt: {0} Ft", Fizetes);
+        }
+
+        private static void Feladat1Munk()
+        {
+            Console.WriteLine("Töltsük fel a tömböt véletlenszámokkal a megadott tartományból");
+            for (int i = 0; i < Munkaido.Length; i++)
+            {
+                Munkaido[i] = rnd.Next(6, 15);
+            }
+            for (int j = 0; j < Munkaido.Length; j++)
+            {
+                Console.WriteLine("\t{0}. nap -> {1}",j+1,Munkaido[j]);
+            }
+            int k = 0;
+           /* foreach (var Munka in Munkaido)
+            {
+                k++;
+                Console.WriteLine("\t{0:00}. nap -> {1}", k, Munka);
+            }*/
+        }
+
+        private static void KesesekFeladat()
+        {
+            Feladat1Kes(); Console.WriteLine("\n--------------------------------\n");
+            Feladat2Kes(); Console.WriteLine("\n--------------------------------\n");
+            Feladat3Kes(); Console.WriteLine("\n--------------------------------\n");
+            Feladat4Kes(); Console.WriteLine("\n--------------------------------\n");
+            Feladat5Kes(); Console.WriteLine("\n--------------------------------\n");
+            Feledat67Kes(); Console.WriteLine("\n--------------------------------\n");
+            Feladat8Kes(); Console.WriteLine("\n--------------------------------\n");
+        }
+
+        private static void Feladat8Kes()
+        {
+            Console.WriteLine("Írjuk ki az értékeket növekvő rendben!");
+            int CsereKeses;
+            for (int i = 0; i < KesesekTMB.Length-1; i++)
+            {
+                for (int j = 0; j < KesesekTMB.Length-1; j++)
+                {
+                    if(KesesekTMB[j]>KesesekTMB[j+1])
+                    {
+                        CsereKeses = KesesekTMB[j];
+                        KesesekTMB[j] = KesesekTMB[j + 1];
+                        KesesekTMB[j + 1] = CsereKeses;
+                    }
+                }
+            }
+            for (int i = 0; i < KesesekTMB.Length; i++)
+            {
+                if (i % 8 == 0)
+                {
+                    Console.Write("\n");
+                }
+                Console.Write("\t{0} ", KesesekTMB[i]);
+            }
+        }
+
+        private static void Feledat67Kes()
+        {
+            Console.WriteLine("Add meg a legkisebb elemet!");
+            int MinKeses = int.MaxValue;
+            int MaxKeses = int.MinValue;
+            for (int i = 0; i < KesesekTMB.Length; i++)
+            {
+                if(KesesekTMB[i]<MinKeses)
+                {
+                    MinKeses = KesesekTMB[i];
+                }
+                if(KesesekTMB[i]>MaxKeses)
+                {
+                    MaxKeses=KesesekTMB[i];
+                }
+            }
+            Console.WriteLine("\tA legkevesebb késé : {0}", MinKeses);
+            Console.WriteLine("\n--------------------------------\n");
+            Console.WriteLine("Add meg a legnagyobb elemet!");
+            Console.WriteLine("\tA legtöbb késé : {0}", MaxKeses);
+        }
+
+        private static void Feladat5Kes()
+        {
+            Console.WriteLine("Add meg, hány embert kell fegyelmire hívni (aki több mint 9-szer késett)");
+            int FegyelmiDb = 0;
+            for (int i = 0; i < KesesekTMB.Length; i++)
+            {
+                if (KesesekTMB[i] >=9)
+                {
+                    FegyelmiDb++;
+                }
+            }
+            Console.WriteLine("\tEnnyi alkalommal lesz fegyelmi: {0}", FegyelmiDb);
+        }
+
+        private static void Feladat4Kes()
+        {
+            Console.WriteLine("Add meg, hányan nem késtek egyáltalán");
+            int KesesDb = 0;
+            for (int i = 0; i < KesesekTMB.Length; i++)
+            {
+                if(KesesekTMB[i]==0)
+                {
+                    KesesDb++;
+                }
+            }
+            Console.WriteLine("\tEnnyi alkalommal nem volt késés: {0}", KesesDb);
+        }
+
+        private static void Feladat3Kes()
+        {
+            Console.WriteLine("Add meg az átlagos késést az osztályban");
+            int KesesOsszeg = 0;
+            double KesesAtlag = 0;
+            for (int i = 0; i < KesesekTMB.Length; i++)
+            {
+                KesesOsszeg += KesesekTMB[i];
+            }
+            KesesAtlag = (double)KesesOsszeg / 32;
+            Console.WriteLine("\tA késések összege: {0}",KesesOsszeg);
+            Console.WriteLine("\tA késések átlaga: {0:0.00}", KesesAtlag);
+        }
+
+        private static void Feladat2Kes()
+        {
+            Console.WriteLine("Listázd ki a tömb elemeit egymás mellé, szóközzel elválasztva, úgy hogy egy"+
+                "sorba 8 személy késési adata jelenjen meg.");
+            for (int i = 0; i < 32; i++)
+            {                
+                if (i % 8 == 0)
+                { 
+                    Console.Write("\n");
+                }
+                Console.Write("\t{0} ", KesesekTMB[i]);
+            }
+        }
+
+        private static void Feladat1Kes()
+        {
+            Console.WriteLine("Töltsd fel a tömböt véletlenszámokkal a feladat leírásának megfelelően");
+            int Keses;
+            for (int i = 0; i < 32; i++)
+            {
+                Keses = rnd.Next(0, 21);
+                KesesekTMB[i] = Keses;
+            }
+        }
+    }
+}
---
+++ ConsoleApp8/ConsoleApp8/Program.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static int[] Szamok = new int[30];
+        static int[] KesesekSzama=new int[32];
+        static Random rnd = new Random();
+        static void Main(string[] args)
+        {
+            //BevetelFeladat();
+            KesesekFeladat();
+            Console.ReadKey();
+        }
+
+        private static void KesesekFeladat()
+        {
+            Feladat1Kes(); Console.WriteLine("\n--------------------------------------\n");
+            Feladat3Kes(); Console.WriteLine("\n--------------------------------------\n");
+            Feladat4Kes(); Console.WriteLine("\n--------------------------------------\n");
+            Feladat5Kes(); Console.WriteLine("\n--------------------------------------\n");
+            Feladat67Kes(); Console.WriteLine("\n--------------------------------------\n");
+            Feladat8Kes();
+        }
+
+        private static void Feladat8Kes()
+        {
+            Console.WriteLine("Írjuk ki az értékeket csökkenő rendben! ");
+            int CsereKesesek;
+            for (int i = 0; i < KesesekSzama.Length-1; i++)
+            {
+                for (int j = 0; j < KesesekSzama.Length-1; j++)
+                {
+                    if(KesesekSzama[j]<KesesekSzama[j+1])
+                    {
+                        CsereKesesek = KesesekSzama[j];
+                        KesesekSzama[j] = KesesekSzama[j + 1];
+                        KesesekSzama[j + 1] = CsereKesesek;
+                    }
+                }
+            }
+            
+            for (int i = 0; i < KesesekSzama.Length; i++)
+            {
+                Console.WriteLine("\t{0:00}. diák -> késése: {1} ", i + 1, KesesekSzama[i]);
+            }
+        }
+
+        private static void Feladat67Kes()
+        {
+            Console.WriteLine("Add meg a legkisebb elemet!, Add meg a legnagyobb elemet!");
+            int LegkevesebbKeses=int.MaxValue;
+            int LegtobbKeses=int.MinValue;
+            for (int i = 0; i < KesesekSzama.Length; i++)
+            {
+                if(KesesekSzama[i]<LegkevesebbKeses)
+                {
+                    LegkevesebbKeses = KesesekSzama[i];
+                }
+                if(KesesekSzama[i]>LegtobbKeses)
+                {
+                    LegtobbKeses = KesesekSzama[i];
+                }
+            }
+            Console.WriteLine("\tA legtöbb késés: {0}", LegtobbKeses);
+            Console.WriteLine("\tA legkevesebb keses: {0}", LegkevesebbKeses);
+        }
+
+        private static void Feladat5Kes()
+        {
+            Console.WriteLine("Add meg, hány embert kell fegyelmire hívni (aki több mint 9-szer késett)");
+            int Fegyelmisek = 0;
+            for (int i = 0; i < KesesekSzama.Length; i++)
+            {
+                if(KesesekSzama[i]>9)
+                {
+                    Fegyelmisek++;
+                }
+            }
+            Console.WriteLine("\tEnnyi diák szorul fegyelmire: {0}", Fegyelmisek);
+        }
+
+        private static void Feladat4Kes()
+        {
+            Console.WriteLine("Add meg, hányan nem késtek egyáltalán");
+            int NemKesok = 0;
+            for (int i = 0; i < KesesekSzama.Length; i++)
+            {
+                if(KesesekSzama[i]==0)
+                {
+                    NemKesok++;
+                }
+            }
+            Console.WriteLine("\tEnnyi diák nem késett: {0}", NemKesok);
+        }
+
+        private static void Feladat3Kes()
+        {
+            Console.WriteLine("Add meg az átlagos késést az osztályban");
+            int OsszesKeses = 0;
+            double AtlagKeses = 0;
+            for (int i = 0; i < KesesekSzama.Length; i++)
+            {
+                OsszesKeses += KesesekSzama[i];
+            }
+            AtlagKeses = (double)OsszesKeses / KesesekSzama.Length;
+            Console.WriteLine("ÁTlag késések száma: {0:0.00}", AtlagKeses);
+        }
+
+        private static void Feladat1Kes()
+        {
+            Console.WriteLine("Töltsd fel a tömböt véletlenszámokkal a feladat leírásának megfelelően ");
+            int RandomSzamok = 0;
+            for (int i = 0; i < KesesekSzama.Length; i++)
+            {
+                RandomSzamok = rnd.Next(0, 21);
+                KesesekSzama[i] = RandomSzamok;
+            }
+            Console.WriteLine("\n--------------------------------------\n");
+            for (int i = 0; i < KesesekSzama.Length; i++)
+            {
+                Console.WriteLine("{0:00}. diák -> késése: {1} ",i+1, KesesekSzama[i]);
+            }
+
+        }
+
+        private static void BevetelFeladat()
+        {
+            Feladat1Bev(); Console.WriteLine("\n--------------------------------------\n");
+            Feladat3Bev(); Console.WriteLine("\n--------------------------------------\n");
+            Feladat4Bev(); Console.WriteLine("\n--------------------------------------\n");
+            Feladat5Bev(); Console.WriteLine("\n--------------------------------------\n");
+            Feladat6Bev(); Console.WriteLine("\n--------------------------------------\n");
+            Feladat7Bev(); Console.WriteLine("\n--------------------------------------\n");
+        }
+
+        private static void Feladat7Bev()
+        {
+            Console.WriteLine("Írjuk ki a hőmérséklet értékeket növekvő rendben");
+            int CsereSzam;
+            for (int i = 0; i < Szamok.Length-1; i++)
+            {
+                for (int j = 0; j < Szamok.Length-1; j++)
+                {
+                    if(Szamok[j]>Szamok[j+1])
+                    {
+                        CsereSzam = Szamok[j];
+                        Szamok[j] = Szamok[j + 1];
+                        Szamok[j + 1] = CsereSzam;
+                    }
+                }
+            }
+            for (int j = 0; j < Szamok.Length; j++)
+            {
+                Console.WriteLine("\t{0} nap: {1}", j + 1, Szamok[j]);
+            }
+        }
+
+        private static void Feladat6Bev()
+        {
+            Console.WriteLine("Volt-e 185 a bevétel valamelyik napon?");
+            int db185 = 0;
+            for (int i = 0; i < Szamok.Length; i++)
+            {
+                if(Szamok[i]==185)
+                {
+                    db185++;
+                }
+            }
+            if (db185 > 0) Console.WriteLine("\tVolt ilyen nap");
+            else Console.WriteLine("\tNem volt ilyen nap");
+
+        }
+
+        private static void Feladat5Bev()
+        {
+            Console.WriteLine("Mennyi volt a legalacsonyabb bevétel és a legnagyobb bevétel különbsége?");
+            int MaxBev = int.MinValue;
+            int MinBev = int.MaxValue;
+            for (int i = 0; i < Szamok.Length; i++)
+            {
+                if(Szamok[i]>MaxBev)
+                {
+                    MaxBev = Szamok[i];
+                }
+                if(Szamok[i]<MinBev)
+                {
+                    MinBev = Szamok[i];
+                }
+            }
+            Console.WriteLine("\tA legmagasabb bevétel : {0}", MaxBev);
+            Console.WriteLine("\tA legalacsonyabb bevétel: {0}", MinBev);
+            int Kulonbseg = MaxBev - MinBev;
+            Console.WriteLine("\tA legalacsonyabb bevétel és a legnagyobb bevétel különbsége: {0}", Kulonbseg);
+        }
+
+        private static void Feladat4Bev()
+        {
+            Console.WriteLine("Hányszor volt a napi bevétel 185 és 200 közötti?");
+            int dbVolt = 0;
+            for (int i = 0; i < Szamok.Length; i++)
+            {
+                if(Szamok[i]>185 && Szamok[i]<200)
+                {
+                    dbVolt++;
+                }
+            }
+            Console.WriteLine("Ennyi alkalommal volt a napi bevétel 185 és 200 közötti: {0}", dbVolt);
+        }
+
+        private static void Feladat3Bev()
+        {
+            Console.WriteLine("Adjuk meg az átlagos novemberi bevételt! Az értéket 1 tizedesre kerekítsük!");
+            int OsszegBev = 0;
+            double AtlagBev = 0;
+            for (int i = 0; i < Szamok.Length; i++)
+            {
+                OsszegBev += Szamok[i];
+            }
+            AtlagBev =(double) OsszegBev / Szamok.Length;
+            Console.WriteLine("\tÁtlag bevétel: {0:0.00}", AtlagBev);
+        }
+
+        private static void Feladat1Bev()
+        {
+            Console.WriteLine("Töltsük fel a tömböt véletlenszámokkal [125,278] tartományból");
+            for (int i = 0; i < Szamok.Length; i++)
+            {
+                Szamok[i] = rnd.Next(125, 279);
+            }
+            for (int j = 0; j < Szamok.Length; j++)
+            {
+                Console.WriteLine("\t{0} nap: {1}", j + 1, Szamok[j]);
+            }
+        }
+    }
+}
